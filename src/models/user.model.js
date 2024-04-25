99e36630e31c130e63e0409acca6a8ed03019401
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-04-25 21:39:32 +0530

    add db connection and user model

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVarified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = models.users || model("users", userSchema);
+export default User;
