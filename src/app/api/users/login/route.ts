304da72ff56ffed3c5bf9ad0e32d7a466810da16
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-02 01:13:43 +0530

    add login api

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,50 @@
+import { connectDB } from "@/db";
+import User from "@/models/user.models";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = request.json();
+    const { email, password }: any = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
+    }
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+      token,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
