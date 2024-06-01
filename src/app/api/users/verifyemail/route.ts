ddf93baae6578c8179806f0e21f5748f17af7073
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-02 00:50:34 +0530

    add verifyemail api

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,39 @@
+import { connectDB } from "@/db";
+import User from "@/models/user.models";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: {
+        $gt: Date.now(),
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      {
+        message: "User verified successfully",
+        success: true,
+      },
+      { status: 500 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
