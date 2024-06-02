c8a0b07fb2f88d846b0bdcb70c656de4f7e0c52b
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-02 23:34:43 +0530

    refactor login api

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -8,27 +8,34 @@ connectDB();
 
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = request.json();
-    const { email, password }: any = reqBody;
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
 
+    //check if user exists
     const user = await User.findOne({ email });
-
     if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 400 });
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
     }
+    console.log("user exists");
 
+    //check if password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
-
     if (!validPassword) {
       return NextResponse.json({ error: "Invalid password" }, { status: 400 });
     }
+    console.log(user);
 
+    //create token data
     const tokenData = {
       id: user._id,
       username: user.username,
       email: user.email,
     };
-
+    //create token
     const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
       expiresIn: "1d",
     });
@@ -36,15 +43,12 @@ export async function POST(request: NextRequest) {
     const response = NextResponse.json({
       message: "Login successful",
       success: true,
-      token,
     });
-
     response.cookies.set("token", token, {
       httpOnly: true,
     });
-
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
