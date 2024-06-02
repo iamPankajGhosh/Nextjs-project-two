1524345f50df4d0f85e80a59fe074abf08e04f23
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-06-02 20:13:29 +0530

    add profile api

---
+++ src/app/api/users/profile/route.ts
@@ -0,0 +1,29 @@
+import { connectDB } from "@/db";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/user.models";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: "User found",
+        data: user,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
